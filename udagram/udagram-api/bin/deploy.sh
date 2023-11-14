160e8b225cf05f0d5c09ce54322be55ec11e49f6
Haneen Alnuwaysir <haneenalnuwaysir@gmail.com>
2023-11-14 22:44:00 +0300

    config

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,6 +1,6 @@
 # cd ./www/
-# eb init --platform node.js-14 --region us-east-1 udagram-api
-# eb use udagram-api-dev
+eb init --platform node.js-14 --region us-east-1 udagram-api
+eb use udagram-api-dev
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_HOST=$POSTGRES_HOST POSTGRES_DB=$POSTGRES_DB AWS_BUCKET=$AWS_BUCKET AWS_REGION=$AWS_REGION AWS_PROFILE=$AWS_PROFILE JWT_SECRET=$JWT_SECRET URL=$URL PORT=$PORT USERNAME=$USERNAME
 
 # eb deploy 
